05d6b78b186e23b2da58ff808d849d75a2940835
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-18 16:41:28 +0100

    50. Structure

---
+++ 05-search-github-users/src/components/user/UserProfile.tsx
@@ -0,0 +1,13 @@
+type UserProfileProps = {
+  userName: string;
+};
+
+const UserProfile = ({ userName }: UserProfileProps) => {
+  return (
+    <h2 className='text-2xl font-bold'>
+      {userName}
+    </h2>
+  );
+};
+
+export default UserProfile;
