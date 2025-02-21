8cfb8b236da22a87e4d1ac8552919d2454d2bf35
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-21 23:15:03 +0100

    58. Most Used Languages

--- 05-search-github-users/src/utils.ts
+++ 05-search-github-users/src/utils.ts
@@ -35,3 +35,36 @@ export const calculateMostStarredRepos = (
 
   return starredRepos;
 };
+
+export const calculatePopularLanguages = (
+  repositories: Repository[]
+): { language: string; count: number }[] => {
+  if (repositories.length === 0) {
+    return [];
+  };
+
+  const languageMap: { [key: string]: number } = {};
+
+  repositories.forEach((repo) => {
+    if (repo.languages.edges.length === 0) {
+      return;
+    };
+
+    repo.languages.edges.forEach((language) => {
+      const { name } = language.node;
+      languageMap[name] = (languageMap[name] || 0) + 1;
+    });
+  });
+
+  if (Object.keys(languageMap).length === 0) {
+    return [];
+  };
+
+  return (
+    Object
+      .entries(languageMap)
+      .sort(([, a], [, b]) => b - a)
+      .slice(0, 5)
+      .map(([language, count]) => ({ language, count }))
+  );
+};
