b794eca67721bac7b23c4c1e3ee8f7f70cde8d60
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-22 13:21:35 +0100

    59. Charts

--- 05-search-github-users/src/components/charts/UsedLanguages.tsx
+++ 05-search-github-users/src/components/charts/UsedLanguages.tsx
@@ -1,12 +1,51 @@
 import { type Repository } from '@/types';
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from 'recharts';
+import {
+  type ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent
+} from '@/components/ui/chart';
 import { calculatePopularLanguages } from '@/utils';
 
-const UsedLanguages = ({ repositories }: { repositories: Repository[]}) => {
+const UsedLanguages = ({ repositories }: { repositories: Repository[] }) => {
   const popularLanguages = calculatePopularLanguages(repositories);
-  console.log(popularLanguages);
+
+  const chartConfig = {
+    language: {
+      label: 'Language',
+      color: '#2563eb',
+    },
+  } satisfies ChartConfig;
 
   return (
-    <div>UsedLanguages</div>
+    <div>
+      <h2 className='text-2xl font-semibold text-center mb-4'>
+        Used Languages
+      </h2>
+
+      <ChartContainer config={chartConfig} className='h-100 w-full'>
+        <BarChart accessibilityLayer data={popularLanguages}>
+          <CartesianGrid vertical={false} />
+
+          <XAxis
+            dataKey='language'
+            tickLine={false}
+            tickMargin={10} 
+          />
+
+          <YAxis dataKey='count' />
+
+          <ChartTooltip content={<ChartTooltipContent />} />
+
+          <Bar 
+            dataKey='count' 
+            fill='var(--color-language)' 
+            radius={4} 
+          />
+        </BarChart>
+      </ChartContainer>
+    </div>
   );
 };
 
