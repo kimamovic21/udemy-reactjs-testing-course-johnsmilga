95ab46845b021eb48e832ce59350a7b05d12fbf1
kimamovic21 <imamovic.kerim@gmail.com>
2025-03-04 14:06:08 +0100

    69. ForkedRepos Chart Component Test

---
+++ 05-search-github-users/src/__tests__/ForkedRepos.test.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
+import { mockRepositories } from './utils.test';
+import ForkedRepos from '@/components/charts/ForkedRepos';
+
+vi.mock('@/components/ui/chart', () => {
+  return {
+    ChartContainer: ({ children }: { children: React.ReactNode }) => (
+      <div>{children}</div>
+    ),
+    ChartTooltip: ({ content }: { content: React.ReactNode }) => (
+      <div>{content}</div>
+    ),
+    ChartTooltipContent: () => <div>Tooltip Content</div>,
+  };
+});
+
+vi.mock('recharts', () => {
+  return {
+    BarChart: ({ children }: { children: React.ReactNode }) => (
+      <div>{children}</div>
+    ),
+    CartesianGrid: () => <div>CartesianGrid</div>,
+    XAxis: () => <div>XAxis</div>,
+    YAxis: () => <div>YAxis</div>,
+    Bar: () => <div>Bar</div>,
+  };
+});
+
+describe('ForkedRepos', () => {
+  beforeEach(() => {
+    render(<ForkedRepos repositories={mockRepositories} />);
+  });
+
+  test('should render the ForkedRepos component', () => {
+    expect(screen.getByText('Forked Repos')).toBeInTheDocument();
+  });
+
+  test('should render the chart with correct data', () => {
+    expect(screen.getByText('CartesianGrid')).toBeInTheDocument();
+    expect(screen.getByText('XAxis')).toBeInTheDocument();
+    expect(screen.getByText('YAxis')).toBeInTheDocument();
+    expect(screen.getByText('Bar')).toBeInTheDocument();
+    expect(screen.getByText('Tooltip Content')).toBeInTheDocument();
+  });
+});
